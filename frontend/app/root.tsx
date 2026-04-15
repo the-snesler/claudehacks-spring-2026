8e9478d5e8bcdedeaf3e39e3736bb8300c5f5651
Sam N <78718829+TetraTsunami@users.noreply.github.com>
2026-04-14 19:26:10 -0500

    fix icon

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -32,6 +32,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#c5050c" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="apple-mobile-web-app-title" content="Bike Benefits" />
         <Meta />
         <Links />
       </head>
